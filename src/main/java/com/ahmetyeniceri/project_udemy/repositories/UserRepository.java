fceef92c1b35993d9bb62ce76ff26b54ecca6c89
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-07-23 15:43:53 +0300

    ahmet/created-repository

---
+++ src/main/java/com/ahmetyeniceri/project_udemy/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ahmetyeniceri.project_udemy.repositories;
+
+import com.ahmetyeniceri.project_udemy.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
