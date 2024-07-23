a929abf61599d7e2153cf542e34ce5d85b0b89ce
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-07-23 15:40:16 +0300

    Initial commit

---
+++ src/main/java/com/ahmetyeniceri/project_udemy/ProjectUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetyeniceri.project_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmetyeniceri/project_udemy/ProjectUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetyeniceri.project_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
